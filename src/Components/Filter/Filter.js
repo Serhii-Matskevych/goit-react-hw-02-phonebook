f5f1c9faba6103f1adb805015293a6f2c15fbec6
Serhii-Matskevych <sergeyma1992@gmail.com>
2022-10-01 17:40:40 +0300

    1

---
+++ src/Components/Filter/Filter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Filter.module.css';
+
+const Filter = ({ filter, searchContact }) => {
+  return (
+    <label className={styles.label}>
+      {' '}
+      Find contacts by name
+      <input
+        className={styles.input}
+        onChange={event => searchContact(event.target.value)}
+        value={filter}
+      />
+    </label>
+  );
+};
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  searchContact: PropTypes.func.isRequired,
+};
+
+export default Filter;
